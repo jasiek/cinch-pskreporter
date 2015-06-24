526b3dbaa458b9288b092ec595ad8908883cd0f1
Jan Szumiec <jan.szumiec@gmail.com>
2015-06-24 23:31:46 +0100

    Updated spec.

--- spec/cinch-pskreporter_spec.rb
+++ spec/cinch-pskreporter_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe Cinch::Plugins::PSKReporter do
   include Cinch::Test
+
+  before :all do
+    @bot = make_bot(Cinch::Plugins::PSKReporter,
+                    { callsigns: ['2e0kef'],
+                      irc_username: 'DUMMY',
+                      irc_password: 'DUMMY' })
+  end
+
+  it "should attempt to contact pskreporter and retrieve reception reports for 2E0KEF" do
+    # TODO
+  end
 end
